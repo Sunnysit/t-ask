73465734ee639d2dad65e29e53a8641f800d2d49
KarlaLGA <KarlaLGA@users.noreply.github.com>
2019-10-11 15:42:38 -0700

    Update ContactForm.jsx

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 
 const ContactForm = () => {
     return ( 
+        //only when we cross the state between components, we use redux
+        //
+
+
+
+
+        //create local state
+        //create function for submit, prevent default. 
+            //create validate trim and make a condition to check if something is in the input
+            //update component
+            //error message, display at the end of the form
+            //successful message, delete form if successful
+            //use terniary syntax
+
         <div className="">
             <h1>ContactForm Component</h1>
             <form action="">
@@ -9,10 +23,14 @@ const ContactForm = () => {
                 <input type="text" name="name" id="name"/>
                 <label htmlFor="email">Email</label>
                 <input type="text"/>
-                <label htmlFor="subject">Subject</label>
-                <input type="text" name="subject" id="subject"/>
-                <label htmlFor="body">Body</label>
-                <textarea name="body" id="body" cols="30" rows="10"></textarea>
+                <label htmlFor="inquiry">Inquiry</label>
+                <select name="" id="inquiry">
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                </select>
+                <label htmlFor="message">Message</label>
+                <textarea name="message" id="message" cols="30" rows="10"></textarea>
             </form>
         </div>
      );
