79689d8f5fe7f6e5f5a4771e1f1e1d7e7c4b5af5
KarlaLGA <KarlaLGA@users.noreply.github.com>
2019-10-11 10:12:29 -0700

    Add contact form

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -4,6 +4,16 @@ const ContactForm = () => {
     return ( 
         <div className="">
             <h1>ContactForm Component</h1>
+            <form action="">
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" id="name"/>
+                <label htmlFor="email">Email</label>
+                <input type="text"/>
+                <label htmlFor="subject">Subject</label>
+                <input type="text" name="subject" id="subject"/>
+                <label htmlFor="body">Body</label>
+                <textarea name="body" id="body" cols="30" rows="10"></textarea>
+            </form>
         </div>
      );
 }
