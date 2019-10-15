9f5b012c9102ff5e879bdc8962e55b54698c3480
KarlaLGA <KarlaLGA@users.noreply.github.com>
2019-10-15 00:45:37 -0700

    Add condition for contact form

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -100,24 +100,32 @@ const ContactForm = () => {
     }
     return (
         <div className="contactBody">
-            <form action="/contact-message" onSubmit={ whenSubmit } className="form">
-                <label htmlFor="name">Name</label>
-                <input type="text" name="name" id="name" onChange={(e) => {setForm({...form, name: e.target.value})}}/>
-                <div className="errorMessage">{ error.errorName }</div>
-                <label htmlFor="email">Email</label>
-                <input type="text" name="email" id="email" onChange={(e) => {setForm({...form, email: e.target.value})}}/>
-                <div className="errorMessage">{ error.errorEmail }</div>
-                <label htmlFor="inquiry">Inquiry</label>
-                <select name="" id="inquiry" onChange={(e) => {setForm({...form, inquiry: e.target.value})}}>
-                    <option value="general">General</option>
-                    <option value="content">Content</option>
-                    <option value="other">Other</option>
-                </select>
-                <label htmlFor="message">Message</label>
-                <textarea name="message" id="message" cols="30" rows="10" onChange={(e) => {setForm({...form, message: e.target.value})}}></textarea>
-                <div className="errorMessage">{ error.errorMessage }</div>
-                <button>Send</button>
-            </form>
+            {!sentState ? (
+                <form action="/contact-message" onSubmit={ whenSubmit } className="form">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" id="name" onChange={(e) => {setForm({...form, name: e.target.value})}}/>
+                    <div className="errorMessage">{ error.errorName }</div>
+                    <label htmlFor="email">Email</label>
+                    <input type="text" name="email" id="email" onChange={(e) => {setForm({...form, email: e.target.value})}}/>
+                    <div className="errorMessage">{ error.errorEmail }</div>
+                    <label htmlFor="inquiry">Inquiry</label>
+                    <select name="" id="inquiry" onChange={(e) => {setForm({...form, inquiry: e.target.value})}}>
+                        <option value="general">General</option>
+                        <option value="content">Content</option>
+                        <option value="other">Other</option>
+                    </select>
+                    <label htmlFor="message">Message</label>
+                    <textarea name="message" id="message" cols="30" rows="10" onChange={(e) => {setForm({...form, message: e.target.value})}}></textarea>
+                    <div className="errorMessage">{ error.errorMessage }</div>
+                    <button>Send</button>
+                </form>
+            ) : (
+                <div className="message">
+                    <h2>Thank you for your message!</h2>
+                    <p>Our team will contact you shortly</p>
+                </div>
+            )}
+            
         </div>
      );
 }
