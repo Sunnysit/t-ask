89e6e29c0b7f11166ff4cb4c2ff2d6f831580560
KarlaLGA <KarlaLGA@users.noreply.github.com>
2019-10-18 00:19:45 -0700

    Modify select tag to button and ul

--- src/components/home/comparison/SelectLanguages.jsx
+++ src/components/home/comparison/SelectLanguages.jsx
@@ -1,32 +1,49 @@
-import React from 'react';
-
-const languages = [
-    'python',
-    'java',
-    'javascript',
-    'c#',
-    'php',
-    'c++',
-    'r',
-    'objective-c',
-    'swift',
-    'matlab',
-    'typescript',
-    'kotlin',
-    'ruby',
-    'visual basic',
-    'go',
-    'rust'
-]
-
-let optionsLanguages = languages.map(language => <option key={language} value={language}>{language}</option>)
+import React, {useState} from 'react';
 
 const SelectLanguage = () => {
+    const languages = [
+        'python',
+        'java',
+        'javascript',
+        'c#',
+        'php',
+        'c++',
+        'r',
+        'objective-c',
+        'swift',
+        'matlab',
+        'typescript',
+        'kotlin',
+        'ruby',
+        'visual basic',
+        'go',
+        'rust'
+    ]
+    
+    const [displayDropDown, setDisplayDropDown] = useState(true);
+    
+    
+    
+    let handleDropDown = () => {
+        setDisplayDropDown(!displayDropDown)
+    }
+    
+    let optionsLanguages = languages.map(language => <li key={language} value={language}>{language}</li>)
+    
     return (
-        <div className="">
-            <select name="" id="">
-                { optionsLanguages }
-            </select>
+        <div className="select-language-body">
+            <button onClick={handleDropDown}>Languages</button>
+            {!displayDropDown ? (
+                <div className="drop-down active">
+                    <ul className="languages">
+                    { optionsLanguages }
+                    </ul>
+                </div>
+            ) : (
+                <div className="drop-down"></div>
+            )}
+            
+            
         </div>
     )
 }
