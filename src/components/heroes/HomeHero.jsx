43d6b45144a2510d3c4031af09c951aa88b25ab9
KarlaLGA <KarlaLGA@users.noreply.github.com>
2019-10-21 16:21:35 -0700

    Move comparison feature to hero component

--- src/components/heroes/HomeHero.jsx
+++ src/components/heroes/HomeHero.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Comparison from '../home/comparison/Comparison'
+import LineGraph from '../home/comparison/LineGraph'
 
 const HomeHero = () => {
     return (
@@ -7,6 +9,11 @@ const HomeHero = () => {
             <p>Here we explain how the comparison works</p>
             <div className="placeholder placeholder-comparison-feature">
                 <p>Here is the comparison feature</p>
+                <Comparison/>
+                <div className="graph-container">
+                <LineGraph/>
+
+                </div>
             </div>
         </div>
     )
