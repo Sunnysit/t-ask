e0ae4926a38f4c1ecf010ffafdb58b6591b92464
Sunny Xue <sunny.sit26@gmail.com>
2019-10-29 15:34:29 -0700

    Update Header & Footer

--- src/components/footer/NewsLetter.jsx
+++ src/components/footer/NewsLetter.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const NewsLetter = () => {
-    return ( 
+
+    const [email,setEmail] = useState("");
+    const [msg,setMsg] = useState("");
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        let inputEmail = email.trim();
+        console.log(inputEmail)
+        if(inputEmail&&inputEmail.length>=1)
+        {
+            setMsg("");
+        }
+        else{
+            setMsg('Please input valid email.');
+        }
+
+    }
+
+    return (
         <section className="newsletter-section">
-            <h2>This is newsletter</h2>
+            <div className="newsletter-container">
+                <p className="newsletter-description">Subscribe to our newsletter to receive the latest news</p>
+                <form onSubmit={handleOnSubmit} action="#" method="POST" className="subscribe-form">
+                    <input
+                        placeholder="email@task.com"
+                        type="email"
+                        name="subscribe-email"
+                        id="subscribe-email"
+                        value={email}
+                        onChange={e=>setEmail(e.target.value)}
+                        />
+                    <button className="btn subscribe-btn" type="submit">Subscribe</button>
+                </form>
+                <p className="subscribe-message">{msg}</p>
+            </div>
         </section>
-     );
+    );
 }
- 
+
 export default NewsLetter;
