46c4548a7ba288085b20f1f7c9da94a8812e1cc3
Sunny Xue <sunny.sit26@gmail.com>
2019-11-01 12:07:55 -0700

    Fix tiny css bug

--- src/components/articles/ArticleItem.jsx
+++ src/components/articles/ArticleItem.jsx
@@ -9,7 +9,7 @@ const ArticleItem = (props) => {
         <li className="article-list-item">
             <article className="article">
                 <h2>
-                    <a target="_blank" href={article.link}>{article.title}</a>
+                    <a rel="noopener noreferrer" target="_blank" href={article.link}>{article.title}</a>
                 </h2>
                 <p>{article.author}</p>
                 <p>{article.date}</p>
