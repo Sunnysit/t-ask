08b7f725b6d28ca3694f3399cd679fdc912a6cd9
Sunny Xue <sunny.sit26@gmail.com>
2019-11-04 14:48:45 -0800

    Add Carousel to HeroArticle

--- src/components/heroes/HeroArticle.jsx
+++ src/components/heroes/HeroArticle.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 const HeroArticle = () => {
+
+
     return(
         <div className="hero-article hero">
-            <div className="">
-                <p>Here goes the featured article</p>
-            </div>
+            <Carousel
+                showThumbs={false}
+                infiniteLoop={true}
+                showStatus={false}
+            >
+                <div>
+                    <img src="https://cdn-images-1.medium.com/fit/t/1600/480/0*qASU92GfMj2HCTMg.jpg" alt="java"/>
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src="https://cdn-images-1.medium.com/fit/t/1600/480/1*E8dtcW7fEoJc7qw20_rTKg.jpeg" alt="python"/>
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                    <img src="https://cdn-images-1.medium.com/fit/t/1600/480/0*Ki5tlWeeJf2BR0sG" alt="javascript"/>
+                    <p className="legend">Legend 3</p>
+                </div>
+            </Carousel>
         </div>
     )
 }
