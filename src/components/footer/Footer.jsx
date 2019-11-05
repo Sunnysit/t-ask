197c6d2f1012bd94c483b5c3b39fd72bda30b176
Sunny Xue <sunny.sit26@gmail.com>
2019-11-05 11:21:23 -0800

    Update Footer style

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,11 +7,12 @@ const Footer = () => {
     <footer className="footer">
         <NewsLetter />
         <nav className="footer-navigation">
+            <p className="navigation-description">Proudly Powered by:</p>
             <ul className="navigation">
-                <li className="navigation-item"><a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/" className="footer-link">LinkedIn</a></li>
-                <li className="navigation-item"><a rel="noopener noreferrer" target="_blank" href="https://www.facebook.com/" className="footer-link">Facebook</a></li>
-                <li className="navigation-item"><a rel="noopener noreferrer" target="_blank" href="https://twitter.com/" className="footer-link">Twitter</a></li>
-                <li className="navigation-item"><a rel="noopener noreferrer" target="_blank" href="#term" className="footer-link">Terms and conditions</a></li>
+                <li className="navigation-item"><a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/" className="footer-link"><img src="./assets/company/linkup.png" alt="linkup logo"/></a></li>
+                <li className="navigation-item"><a rel="noopener noreferrer" target="_blank" href="https://www.facebook.com/" className="footer-link"><img src="./assets/company/googletrends.png" alt="google trends logo"/></a></li>
+                <li className="navigation-item"><a rel="noopener noreferrer" target="_blank" href="https://twitter.com/" className="footer-link"><img src="./assets/company/github.png" alt="Github logo"/></a></li>
+                <li className="navigation-item"><a rel="noopener noreferrer" target="_blank" href="#term" className="footer-link"><img src="./assets/company/medium.png" alt="medium logo"/></a></li>
                 <li className="navigation-item"><a rel="noopener noreferrer" target="_blank" href="https://www.meetup.com/" className="footer-link"><img src="./assets/company/meetup.png" alt="meetup logo"/></a></li>
             </ul>
         </nav>
