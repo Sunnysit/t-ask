0113812b43c5c5cc5066f03cc5f5597a85902300
KarlaLGA <KarlaLGA@users.noreply.github.com>
2019-11-11 14:51:48 -0800

    Update bar graph legends

--- src/components/home/comparison/Jobs/BarGraphJobsLegend.jsx
+++ src/components/home/comparison/Jobs/BarGraphJobsLegend.jsx
@@ -1,25 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 const BarGraphJobsLegend = () => {
 
     const languages = useSelector(state => state.languages.selectedLanguages);
-    const firstLang = languages[0].languageName;
-    console.log(firstLang);
-    const secondLang = languages[1].languageName;
-    console.log(secondLang);
-    const thirdLang = languages[2].languageName;
-    console.log(thirdLang);
+    const [firstLang, setFirstLang] = useState('')
+    //console.log(firstLang);
+
+    const [ifSecondLanguage, setIfSecondLanguage] = useState(false);
+    const [secondLang, setSecondLang]= useState('')
+
+    const [ifThirdLanguage, setIfThirdLanguage] = useState(false);
+    const [thirdLang, setThirdLang] = useState('')
+
+    useEffect(() => {
+
+        setFirstLang(languages[0].languageName);
+
+        if(languages[1]){
+            setSecondLang(languages[1].languageName);
+            setIfSecondLanguage(true);
+            //console.log(secondLang);
+        }
+
+        else{
+            setIfThirdLanguage(false);
+        }
+
+        if(languages[2]){
+            setThirdLang(languages[2].languageName);
+            setIfThirdLanguage(true);
+            //console.log(thirdLang);
+        }
+        else{
+            setIfThirdLanguage(false);
+        }
+
+        
+    
+    }, [languages, secondLang, thirdLang])
+
+    
+
     return (
-        <div className="jobs-legend">
-            <div className="third-language">
+        <div className="languages-legend">
+            {ifThirdLanguage ? (
+                <div className="third-language">
                 <p className="color-legend">a</p>
                 <p>{thirdLang}</p>
             </div>
-            <div className="second-language">
-                <p className="color-legend">a</p>
-                <p>{secondLang}</p>
-            </div>
+            ) : (
+            <div className="no-lang"></div>    
+            )}
+
+            {ifSecondLanguage ? (
+                <div className="second-language">
+                    <p className="color-legend">a</p>
+                    <p>{secondLang}</p>
+                </div>
+            ) : (
+                <div className="no-lang"></div>
+            )}
+            
             <div className="first-language">
                 <p className="color-legend">a</p>
                 <p>{firstLang}</p>
