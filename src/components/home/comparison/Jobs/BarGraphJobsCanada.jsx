3e6998c8332d5d9e4cc92bdfca7958579c45d193
KarlaLGA <KarlaLGA@users.noreply.github.com>
2019-11-10 17:23:42 -0800

    Fix indentation

--- src/components/home/comparison/Jobs/BarGraphJobsCanada.jsx
+++ src/components/home/comparison/Jobs/BarGraphJobsCanada.jsx
@@ -6,128 +6,125 @@ import {ResponsiveBar} from '@nivo/bar';
 const BarGraphJobsCanada = () => {
 
     const dataCanada = useSelector(state => state.jobs.jobsCanada);
-    const selectedLanguages = useSelector(state=> state.languages.selectedLanguages);
+    const selectedLanguages = useSelector(state => state.languages.selectedLanguages);
 
-    const [barGraphData, setBarGraphData] = useState([]);
-    const [barGraphKeys, setBarGraphKeys] = useState([]);
+    const [barGraphData,
+        setBarGraphData] = useState([]);
+    const [barGraphKeys,
+        setBarGraphKeys] = useState([]);
 
     useEffect(() => {
-    //normalize data as percentage
-    let mostJobs = 0
-    for (let i = 0; i < dataCanada.length; i++){
-        if(dataCanada[i].totalJobs > mostJobs){
-            mostJobs = dataCanada[i].totalJobs;
+        //normalize data as percentage
+        let mostJobs = 0
+        for (let i = 0; i < dataCanada.length; i++) {
+            if (dataCanada[i].totalJobs > mostJobs) {
+                mostJobs = dataCanada[i].totalJobs;
+            }
         }
-    }
 
-    console.log(mostJobs);
+        //console.log(mostJobs);
 
-    const dataPercentageCanada = dataCanada.map(language => {
-        let percentageJobs = parseFloat(((language.totalJobs * 100) / mostJobs).toFixed(2));
-        return {id_language: language.id_language, name: language.name, totalJobs: percentageJobs}
-    });
+        const dataPercentageCanada = dataCanada.map(language => {
+            let percentageJobs = parseFloat(((language.totalJobs * 100) / mostJobs).toFixed(2));
+            return {id_language: language.id_language, name: language.name, totalJobs: percentageJobs}
+        });
 
-    console.log(dataPercentageCanada);
+        //console.log(dataPercentageCanada); filter data from the selectedLanguages
+        const selectedDataCanada = dataPercentageCanada.filter(language => selectedLanguages.find(lang => lang.languageName === language.name));
 
-    
-        //filter data from the selectedLanguages
-    const selectedDataCanada = dataPercentageCanada.filter(language => selectedLanguages.find(lang => lang.languageName === language.name));
+        //set data format for bar-graph Canada
+        const languageLocationCanada = selectedDataCanada.map(language => {
+            let languageObject = language.name;
+            return {[languageObject]: language.totalJobs}
+        })
+        let languagesLocationCanada = {};
+        for (let i = 0; i < languageLocationCanada.length; i++) {
+            let singleLanguage = languageLocationCanada[i];
+            languagesLocationCanada = {
+                ...languagesLocationCanada,
+                ...singleLanguage
+            }
+        }
+        languagesLocationCanada = {
+            country: 'Canada',
+            ...languagesLocationCanada
+        };
 
+        setBarGraphData([languagesLocationCanada])
 
-    //set data format for bar-graph
-    //Canada
-    const languageLocationCanada=selectedDataCanada.map(language => {
-        let languageObject = language.name;
-        return {[languageObject]: language.totalJobs}
-    })
-    let languagesLocationCanada = {};
-    for(let i = 0; i < languageLocationCanada.length; i++){
-        let singleLanguage = languageLocationCanada[i];
-        languagesLocationCanada = {...languagesLocationCanada, ...singleLanguage}
-    }
-    languagesLocationCanada = {country:'Canada', ...languagesLocationCanada};
+        //console.log(dataCanada);
 
-    setBarGraphData([languagesLocationCanada])
+        const languageKeys = selectedLanguages.map(language => language.languageName);
 
-    //console.log(dataCanada);
+        setBarGraphKeys(languageKeys);
 
-    const languageKeys = selectedLanguages.map(language => language.languageName);
-
-    setBarGraphKeys(languageKeys);
-        
     }, [dataCanada, selectedLanguages])
 
-
-    return (
-            <ResponsiveBar
-                data={barGraphData}
-                keys
-                ={barGraphKeys}
-                indexBy="country"
-                margin={{
-                top: 50,
-                right: 65,
-                bottom: 50,
-                left: 10
-            }}
-                padding={0.1}
-                innerPadding={10}
-                minValue={0}
-                maxValue={100}
-                groupMode='grouped'
-                layout='horizontal'
-                reverse={true}
-                colors={{
-                scheme: 'nivo'
-            }}
-                borderColor="black"
-                axisTop={null}
-                axisRight={null}
-                axisBottom={{
-                tickSize: 5,
-                tickPadding: 5,
-                tickRotation: 0,
-                legend: 'Canada',
-                legendPosition: 'middle',
-                legendOffset: 32
-            }}
-                axisLeft={null}
-                enableGridX={true}
-                labelSkipWidth={12}
-                labelSkipHeight={12}
-                labelTextColor={{
-                theme: 'background'
-            }}
-                legends={[{
-                    dataFrom: 'keys',
-                    anchor: 'right',
-                    direction: 'column',
-                    justify: false,
-                    translateX: 40,
-                    translateY: 0,
-                    itemsSpacing: 40,
-                    itemWidth: 10,
-                    itemHeight: 22,
-                    itemDirection: 'top-to-bottom',
-                    itemOpacity: 0.85,
-                    symbolSize: 12,
-                    effects: [
-                        {
-                            on: 'hover',
-                            style: {
-                                itemOpacity: 1
-                            }
-                        }
-                    ]
+    return (<ResponsiveBar
+        data={barGraphData}
+        keys
+        ={barGraphKeys}
+        indexBy="country"
+        margin={{
+        top: 50,
+        right: 65,
+        bottom: 50,
+        left: 10
+    }}
+        padding={0.1}
+        innerPadding={10}
+        minValue={0}
+        maxValue={100}
+        groupMode='grouped'
+        layout='horizontal'
+        reverse={true}
+        colors={{
+        scheme: 'nivo'
+    }}
+        borderColor="black"
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: 'Canada',
+        legendPosition: 'middle',
+        legendOffset: 32
+    }}
+        axisLeft={null}
+        enableGridX={true}
+        labelSkipWidth={12}
+        labelSkipHeight={12}
+        labelTextColor={{
+        theme: 'background'
+    }}
+        legends={[{
+            dataFrom: 'keys',
+            anchor: 'right',
+            direction: 'column',
+            justify: false,
+            translateX: 40,
+            translateY: 0,
+            itemsSpacing: 40,
+            itemWidth: 10,
+            itemHeight: 22,
+            itemDirection: 'top-to-bottom',
+            itemOpacity: 0.85,
+            symbolSize: 12,
+            effects: [
+                {
+                    on: 'hover',
+                    style: {
+                        itemOpacity: 1
+                    }
                 }
-            ]}
-                
-                animate={true}
-                motionStiffness={90}
-                motionDamping={15}/>
-    )
+            ]
+        }
+    ]}
+        animate={true}
+        motionStiffness={90}
+        motionDamping={15}/>)
 }
-        
- 
 
 export default BarGraphJobsCanada
--- src/components/home/comparison/Jobs/BarGraphJobsUSA.jsx
+++ src/components/home/comparison/Jobs/BarGraphJobsUSA.jsx
@@ -6,102 +6,100 @@ import {ResponsiveBar} from '@nivo/bar';
 const BarGraphJobsUSA = () => {
 
     const dataUsa = useSelector(state => state.jobs.jobsUsa);
-    const selectedLanguages = useSelector(state=> state.languages.selectedLanguages);
+    const selectedLanguages = useSelector(state => state.languages.selectedLanguages);
 
-    const [barGraphData, setBarGraphData] = useState([]);
-    const [barGraphKeys, setBarGraphKeys] = useState([]);
+    const [barGraphData,
+        setBarGraphData] = useState([]);
+    const [barGraphKeys,
+        setBarGraphKeys] = useState([]);
 
     useEffect(() => {
-    //normalize data as percentage
-    let mostJobs = 0
-    for (let i = 0; i < dataUsa.length; i++){
-        if(dataUsa[i].totalJobs > mostJobs){
-            mostJobs = dataUsa[i].totalJobs;
+        //normalize data as percentage
+        let mostJobs = 0
+        for (let i = 0; i < dataUsa.length; i++) {
+            if (dataUsa[i].totalJobs > mostJobs) {
+                mostJobs = dataUsa[i].totalJobs;
+            }
         }
-    }
 
-    console.log(mostJobs);
+        //console.log(mostJobs);
 
-    const dataPercentageUsa = dataUsa.map(language => {
-        let percentageJobs = parseFloat(((language.totalJobs * 100) / mostJobs).toFixed(2));
-        return {id_language: language.id_language, name: language.name, totalJobs: percentageJobs}
-    });
+        const dataPercentageUsa = dataUsa.map(language => {
+            let percentageJobs = parseFloat(((language.totalJobs * 100) / mostJobs).toFixed(2));
+            return {id_language: language.id_language, name: language.name, totalJobs: percentageJobs}
+        });
 
-    console.log(dataPercentageUsa);
-    
-        //filter data from the selectedLanguages
-    const selectedDataUsa = dataPercentageUsa.filter(language => selectedLanguages.find(lang => lang.languageName === language.name));
+        //console.log(dataPercentageUsa); filter data from the selectedLanguages
+        const selectedDataUsa = dataPercentageUsa.filter(language => selectedLanguages.find(lang => lang.languageName === language.name));
 
+        //set data format for bar-graph USA
+        const languageLocationUsa = selectedDataUsa.map(language => {
+            let languageObject = language.name;
+            return {[languageObject]: language.totalJobs}
+        })
+        let languagesLocationUsa = {};
+        for (let i = 0; i < languageLocationUsa.length; i++) {
+            let singleLanguage = languageLocationUsa[i];
+            languagesLocationUsa = {
+                ...languagesLocationUsa,
+                ...singleLanguage
+            }
+        }
+        languagesLocationUsa = {
+            country: 'USA',
+            ...languagesLocationUsa
+        };
 
-    //set data format for bar-graph
-    //USA
-    const languageLocationUsa=selectedDataUsa.map(language => {
-        let languageObject = language.name;
-        return {[languageObject]: language.totalJobs}
-    })
-    let languagesLocationUsa = {};
-    for(let i = 0; i < languageLocationUsa.length; i++){
-        let singleLanguage = languageLocationUsa[i];
-        languagesLocationUsa = {...languagesLocationUsa, ...singleLanguage}
-    }
-    languagesLocationUsa = {country:'USA', ...languagesLocationUsa};
+        setBarGraphData([languagesLocationUsa])
 
+        const languageKeys = selectedLanguages.map(language => language.languageName);
 
-    setBarGraphData([languagesLocationUsa])
+        setBarGraphKeys(languageKeys);
 
-    const languageKeys = selectedLanguages.map(language => language.languageName);
-
-    setBarGraphKeys(languageKeys);
-        
     }, [dataUsa, selectedLanguages])
 
-
-    return (
-            <ResponsiveBar
-                data={barGraphData}
-                keys
-                ={barGraphKeys}
-                indexBy="country"
-                margin={{
-                top: 50,
-                right: 10,
-                bottom: 50,
-                left: 10
-            }}
-                padding={0.1}
-                innerPadding={10}
-                minValue={0}
-                maxValue={100}
-                groupMode='grouped'
-                colors={{
-                scheme: 'nivo'
-            }}
-                borderColor="black"
-                layout='horizontal'
-                axisTop={null}
-                axisRight={null}
-                axisBottom={{
-                tickSize: 5,
-                tickPadding: 5,
-                tickRotation: 0,
-                legend: 'USA',
-                legendPosition: 'middle',
-                legendOffset: 32
-            }}
-                axisLeft={null}
-                enableGridX={true}
-                labelSkipWidth={12}
-                labelSkipHeight={12}
-                labelTextColor={{
-                theme: 'background'
-            }}
-                legends={[]}
-                animate={true}
-                motionStiffness={90}
-                motionDamping={15}/>
-    )
+    return (<ResponsiveBar
+        data={barGraphData}
+        keys
+        ={barGraphKeys}
+        indexBy="country"
+        margin={{
+        top: 50,
+        right: 10,
+        bottom: 50,
+        left: 10
+    }}
+        padding={0.1}
+        innerPadding={10}
+        minValue={0}
+        maxValue={100}
+        groupMode='grouped'
+        colors={{
+        scheme: 'nivo'
+    }}
+        borderColor="black"
+        layout='horizontal'
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: 'USA',
+        legendPosition: 'middle',
+        legendOffset: 32
+    }}
+        axisLeft={null}
+        enableGridX={true}
+        labelSkipWidth={12}
+        labelSkipHeight={12}
+        labelTextColor={{
+        theme: 'background'
+    }}
+        legends={[]}
+        animate={true}
+        motionStiffness={90}
+        motionDamping={15}/>)
 }
-        
- 
 
 export default BarGraphJobsUSA
