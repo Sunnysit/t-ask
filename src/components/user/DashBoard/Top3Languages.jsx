672e648e72a6335b0a775b609289417e2ba93b1b
KarlaLGA <KarlaLGA@users.noreply.github.com>
2019-11-19 02:07:07 -0800

    Style user profile 70%

--- src/components/user/DashBoard/Top3Languages.jsx
+++ src/components/user/DashBoard/Top3Languages.jsx
@@ -1,43 +1,75 @@
-import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import React, {useState, useEffect} from 'react';
+import {useSelector} from 'react-redux';
+import Top3Switch from './Top3Switch';
 import { TaskAxios } from '../../../library/TaskAxios'
+
 
 const Top3Languages = () => {
 
-
     let axiosLibrary = new TaskAxios();
+
+    const languagesStateUsa = useSelector(state => state.languages.languageTrendingDataUsa);
+    const languagesStateCanada = useSelector(state => state.languages.languageTrendingDataCanada);
+    const countryToggle = useSelector(state => state.languages.top3LangToggle);
+
     const user = useSelector(state => state.user.userInfo);
-    const languages = useSelector(state => state.languages.languages);
 
     let userLanguagesId = user.favoriteLanguages;
-
-    let userLanguages;
+    const [userLanguages, setUserLanguages] = useState([])
 
 
     useEffect(() => {
 
-        axiosLibrary.comparisonLangsMenu();
-        
-    }, [axiosLibrary])
+        axiosLibrary.comparisonLangLocation();
 
-    if(userLanguagesId) {
-        userLanguages = languages.filter(language => userLanguagesId.find(lang => lang === language.languageId));
+        //Fetch USA trending
+        if (countryToggle) {
+            if(userLanguagesId) {
+                let rankUserLangs = languagesStateUsa.map((language,index) => {return {...language, rank: index}});
+                let userLangs = rankUserLangs.filter(language => userLanguagesId.find(lang => lang === language.languageId));
+                userLangs.sort((a,b) => a.rank - b.rank);
+                setUserLanguages(userLangs);
+            }
+        } 
+        //Fetch Canada trending;
+        else {
+            if(userLanguagesId) {
+                let rankUserLangs = languagesStateCanada.map((language,index) => {return {...language, rank: index}});
+                let userLangs = rankUserLangs.filter(language => userLanguagesId.find(lang => lang === language.languageId));
+                userLangs.sort((a,b) => a.rank - b.rank);
+                setUserLanguages(userLangs);
+            }
+        }
 
-    }
+    }, [countryToggle, userLanguagesId]);
 
-
-    return(
-        <div className="top-languages">
-            {userLanguages ? (
-                <ul>
-                {userLanguages.map( (languages, index) => <li key={index}>{languages.languageName}</li>)}
-
+    return (
+        <section className="tending-language-section">
+            <p className="comparison-text">Favorite languages ranking</p>
+            <div className="switch-container">
+            <Top3Switch type="language" toggle={countryToggle}/>
+            </div>
+            <div className="trending-language-container">
+                <ul className="trending-language-list">
+                    {userLanguages.map((lang,index) => {
+                        return <li key={lang.languageId} className="tending-language-item">
+                            <p className="language-name">{lang.languageName}</p>
+                            <p className="logo-container">
+                                <span className="rank-text">#{lang.rank}</span>
+                                <img
+                                    className="language-logo"
+                                    src={lang.logoUrl}
+                                    alt={lang.languageName}/>
+                            </p>
+                            <p className="language-description">{lang.languageDescription}</p>
+                        </li>
+                    })
+}
                 </ul>
-            ) : (
-                <ul></ul>
-            )}
-        </div>
-    )
+            </div>
+        </section>
+
+    );
 }
 
 export default Top3Languages;
--- src/views/UserDashboard.jsx
+++ src/views/UserDashboard.jsx
@@ -3,7 +3,7 @@ import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import Settings from '../components/user/DashBoard/Settings';
 import Top3Jobs from '../components/user/DashBoard/Top3Jobs';
-import Top3Languages from '../components/user/DashBoard/Top3Languages';
+import LanguagesSection from '../components/user/DashBoard/LanguagesSection';
 import Axios from 'axios';
 //import Axios from 'axios';
 
@@ -26,7 +26,7 @@ const UserDashboard = () => {
                 console.log(error)
             })
 
-    }, [dispatch])
+    }, [])
 
     console.log(user.name);
 
@@ -36,7 +36,7 @@ const UserDashboard = () => {
             <div className="user-profile-body">
                 <Settings/>
                 <div className="user-profile-content">
-                    <Top3Languages/>
+                    <LanguagesSection/>
                     <Top3Jobs/>
                 </div>
             </div>
