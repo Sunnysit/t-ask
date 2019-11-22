6a56e25ee2cae432219fe098f3ef22a0de360808
KarlaLGA <KarlaLGA@users.noreply.github.com>
2019-11-21 18:30:52 -0800

    Update SignUpBanner.jsx

--- src/components/home/SignUpBanner.jsx
+++ src/components/home/SignUpBanner.jsx
@@ -5,7 +5,7 @@ const SignUpBanner = () => {
     return (
         <div className="sign-up-banner">
             <p>Sign up now and have the information about your favorite languages personalized. </p>
-            <Link to="/register"><button className="btn btn-banner">Sign up</button></Link>
+            <Link to="/register" className="btn-banner"><button className="btn">Sign up</button></Link>
         </div>
     );
 } 
