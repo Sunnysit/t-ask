e2f450e42ea9ebc74dd215a7200d94656efcdf3a
KarlaLGA <KarlaLGA@users.noreply.github.com>
2019-11-26 16:23:58 -0800

    Update about us page

--- src/components/about/ListMembers.jsx
+++ src/components/about/ListMembers.jsx
@@ -6,11 +6,8 @@ const ListMembers = () => {
         //Social Array property not used for now
         {
             name: 'Karla Lopez',
-            role: 'Project Manager/ Front-End Developer',
-            description: 'Graduate in 3D animation. Familiar with modern JavaScript frameworks like Vue.js' +
-                    ' and  React.js. Will be in charge of organizing the team, making sure everything' +
-                    ' is on track. As a  front-end developer will work on the implementation of the d' +
-                    'esign for the website.',
+            role: 'Project Manager & Front-End Developer',
+            description: 'Graduate in 3D animation. Familiar with modern JavaScript frameworks like Vue.js and React.js. In charge of organizing the team, making sure everything is on track. As a front-end developer works on the functionality and implementation of the design for the website.',
             socials: [
                 'LinkedIn', 'GitHub'
             ],
@@ -29,11 +26,8 @@ const ListMembers = () => {
             ]
         }, {
             name: 'Sunny Xue',
-            role: 'Lead Developer/ Full-Stack Developer',
-            description: 'Graduate in GIS(Geographic Information System). 2+ years of front-end developmen' +
-                    't experience with HTML, CSS, and JavaScript. Familiar with modern javascript fra' +
-                    'meworks like React.js and Vue.js. Will work on the front-end and back-end of the' +
-                    ' application, while supporting and coordinating the development team.',
+            role: 'Lead Full-Stack Developer',
+            description: 'Graduate in GIS(Geographic Information System). 2+ years of front-end development. Familiar with modern Javascript frameworks like React.js and Vue.js. Works on the front-end and back-end of the application, while coordinating the development team.',
             socials: [
                 'LinkedIn', 'GitHub'
             ],
@@ -53,9 +47,7 @@ const ListMembers = () => {
         }, {
             name: 'Juan Tirado',
             role: 'Full-Stack Developer',
-            description: 'Graduate in Science (Animal Science) and experienced in statistics and genetic r' +
-                    'esearch. As a full-stack developer focused on data analytics, will work on the b' +
-                    'ack-end and assist the implementation of the design for the website.',
+            description: 'Graduate in Science (Animal Science) and experience in statistics using Python and with genetic and molecular biology research. As a full-stack developer focuses on data analytics, works on the back-end with the database and server side using Node.js and Express.',
             socials: [
                 'LinkedIn', 'GitHub'
             ],
@@ -75,9 +67,7 @@ const ListMembers = () => {
         }, {
             name: 'Rafael Montenegro',
             role: 'Full-Stack Developer',
-            description: 'Graduate in Computer Information Systems and Android developer with 4+ years of ' +
-                    'experience. Is familiar with modern web frameworks such as React.js. Will be imp' +
-                    'lementing both front-end and back-end aspects of the application. ',
+            description: 'Graduate in Computer Information Systems and Android developer with 4+ years of experience. Is familiar with modern web frameworks such as React.js and Express.js. Implements the back-end of the application, while assisting the front-end.',
             socials: [
                 'LinkedIn', 'GitHub'
             ],
@@ -96,11 +86,8 @@ const ListMembers = () => {
             ]
         }, {
             name: 'Luis Eduardo Consolo',
-            role: 'Lead Designer/ UX and UI Designer',
-            description: 'Graduate in Graphic Design worked in the publishing field for 3+ years, studied ' +
-                    'drawing,  watercolor and mixed arts. Will be in charge of approving the UI and s' +
-                    'upervise UX,  guaranteeing that the final product is consistent with the design ' +
-                    'proposed.',
+            role: 'Lead UX/UI Designer',
+            description: 'Graduate in Graphic Design worked in the publishing field for 3+ years, studied drawing, watercolor and mixed arts. In charge of approving the UI and supervising UX, guaranteeing that the final product is consistent with the design proposed.',
             socials: [
                 'LinkedIn', 'Behance'
             ],
@@ -119,11 +106,8 @@ const ListMembers = () => {
             ]
         }, {
             name: 'Diana Gunawan',
-            role: 'UX and UI Designer/ Business Strategist',
-            description: 'Graduate in Business majoring in Marketing. With 5+ years of experience in servi' +
-                    'ce and fast-moving consumer goods industry. Enthusiastic about UI & UX, will be ' +
-                    'responsible for contributing to the design team by providing the elements needed' +
-                    ' for the project.',
+            role: 'UX/UI Designer & Business Strategist ',
+            description: 'Graduate in Business with a major in Marketing. Has 5+ years of experience in marketing and retail industry. Enthusiastic about UI & UX, responsible for contributing to the design team by providing the elements needed for the project. ',
             socials: [
                 'LinkedIn', 'Behance'
             ],
@@ -142,10 +126,8 @@ const ListMembers = () => {
             ]
         }, {
             name: 'Satnam Singh Sandhu',
-            role: 'UX and UI Designer/Front End Developer',
-            description: 'Graduate in Computer Science. Able to complete layout designs and front end codi' +
-                    'ng for a  website. Desire to learn from any source, either from school programs ' +
-                    'or online. Will be responsible for assisting the design team.',
+            role: 'UX/UI Designer & Front-End Developer',
+            description: 'Graduate in Computer Science and Mathematics. Able to complete layout designs and front-end coding for websites. Desire to learn from any source, either from school programs or online. Responsible for assisting the design team. ',
             socials: [
                 'LinkedIn', 'Behance'
             ],
@@ -166,7 +148,7 @@ const ListMembers = () => {
     ]
 
     return (
-        <div className="team-members">
+        <div className="team-members card">
             {teamMembers.map(teamMember => (<TeamMember key={teamMember.name} teamMember={teamMember}/>))
 }
         </div>
